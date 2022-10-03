80f15f3ea7722f229bf9b959a6adac4b5f6d12d5
Rostislav <rvssv@bk.ru>
2022-10-03 19:38:05 +0300

    added Main Page

---
+++ src/components/main/MainTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="title-wrapper">
+        <h1 class="title">
+            Рассчитайте стоимость автомобиля в лизинг
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MainTitle"
+    }
+</script>
+
+<style lang="scss">
+    .title {
+        font-size: $bigFontSize;
+        font-weight: $super;
+        font-style: normal;
+        line-height: $bigLineHeight;
+    }
+</style>
